e8d904efc70af418768f10ccd68b8d52e23e3bcc
Russell Brazell <brazellrussell@gmail.com>
2021-04-12 13:36:34 -0600

    starting to modifiy layout to include all the new design features

--- app/src/main/java/edu/cnm/deepdive/northstarsharingclient/controller/activity/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/northstarsharingclient/controller/activity/MainActivity.java
@@ -5,11 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import edu.cnm.deepdive.northstarsharingclient.R;
 import edu.cnm.deepdive.northstarsharingclient.service.GoogleSignInService;
@@ -20,20 +18,16 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    BottomNavigationView navView = findViewById(R.id.nav_view);
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
-    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-        .build();
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-    NavigationUI.setupWithNavController(navView, navController);
+    NavigationUI.setupActionBarWithNavController(this, navController);
+    NavigationUI.setupWithNavController( navController);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.main_options, menu);
+    getMenuInflater().inflate(R.menu.main_options_menu, menu);
     return true;
   }
 
---
+++ app/src/main/java/edu/cnm/deepdive/northstarsharingclient/controller/fragment/ImageListFragment.java
@@ -0,0 +1,36 @@
+package edu.cnm.deepdive.northstarsharingclient.controller.fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import edu.cnm.deepdive.northstarsharingclient.R;
+import edu.cnm.deepdive.northstarsharingclient.databinding.FragmentImageListBinding;
+
+
+public class ImageListFragment extends Fragment {
+
+  private FragmentImageListBinding binding;
+  public ImageListFragment() {
+    // Required empty public constructor
+  }
+
+  public static ImageListFragment newInstance(String param1, String param2) {
+    ImageListFragment fragment = new ImageListFragment();
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    binding = FragmentImageListBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+}
