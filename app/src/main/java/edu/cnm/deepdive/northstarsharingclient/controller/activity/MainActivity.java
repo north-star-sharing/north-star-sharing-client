2db77644628455e2886f092d4d86b8ccbd512f0a
Russell Brazell <brazellrussell@gmail.com>
2021-04-13 08:27:47 -0600

    starting to work on navigation

--- app/src/main/java/edu/cnm/deepdive/northstarsharingclient/controller/activity/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/northstarsharingclient/controller/activity/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
     getMenuInflater().inflate(R.menu.main_options_menu, menu);
     return true;
   }
