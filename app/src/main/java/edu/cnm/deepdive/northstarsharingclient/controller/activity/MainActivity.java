5448efa63b92ae9ba9159b25c4e2461c9e0d8ae1
Russell Brazell <brazellrussell@gmail.com>
2021-04-15 13:34:05 -0600

    adding new intent for camera

--- app/src/main/java/edu/cnm/deepdive/northstarsharingclient/controller/activity/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/northstarsharingclient/controller/activity/MainActivity.java
@@ -1,12 +1,15 @@
 package edu.cnm.deepdive.northstarsharingclient.controller.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,12 +17,17 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 import edu.cnm.deepdive.northstarsharingclient.R;
 import edu.cnm.deepdive.northstarsharingclient.service.GoogleSignInService;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+  static final int REQUEST_IMAGE_CAPTURE = 0414;
+  String currentPhotoPath;
   private AppBarConfiguration appBarConfiguration;
   private NavController navController;
 
@@ -28,14 +36,7 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     FloatingActionButton camera = findViewById(R.id.to_camera);
-    camera.setOnClickListener(new View.OnClickListener() {
-      //    TODO  Add intent to go to camera
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
+    camera.setOnClickListener(v -> dispatchTakePictureIntent());
     DrawerLayout drawer = findViewById(R.id.drawer_layout);
     NavigationView navigationView = findViewById(R.id.nav_view);
     appBarConfiguration = new AppBarConfiguration
@@ -81,6 +82,41 @@ public class MainActivity extends AppCompatActivity {
         handled = super.onOptionsItemSelected(item);
     }
     return handled;
+  }
+
+
+  private File createImageFile() throws IOException {
+    // Create an image file name
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    String imageFileName = "CELST_" + timeStamp + "_";
+    File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    File image = File.createTempFile(
+        imageFileName,
+        ".jpg",
+        storageDir
+    );
+    currentPhotoPath = image.getAbsolutePath();
+    return image;
+  }
+
+  private void dispatchTakePictureIntent() {
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+      File photoFile = null;
+      try {
+        photoFile = createImageFile();
+      } catch (IOException ex) {
+        // Error occurred while creating the File
+//          TODO create snackbar
+      }
+      if (photoFile != null) {
+        Uri photoURI = FileProvider.getUriForFile(this,
+            "edu.cnm.deepdive.northstarsharingclient",
+            photoFile);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+      }
+    }
   }
 
 }
