86b2aa09260233c0958f574c0995662ec9795072
Russell Brazell <brazellrussell@gmail.com>
2021-04-15 14:49:09 -0600

    added class for permission dialog

---
+++ app/src/main/java/edu/cnm/deepdive/northstarsharingclient/controller/fragment/PermissionsFragment.java
@@ -0,0 +1,8 @@
+package edu.cnm.deepdive.northstarsharingclient.controller.fragment;
+
+import androidx.fragment.app.DialogFragment;
+
+public class PermissionsFragment extends DialogFragment {
+
+
+}
